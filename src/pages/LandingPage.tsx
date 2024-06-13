133faa297965679753f3e8ca6a4f4050a2faa16c
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2024-06-13 23:54:17 +0200

    landingpage displays animals and you can click on animal to switch route

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,54 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { IAnimal } from "../models/IAnimal";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export const LandingPage = () => {
-  const [animals, setAnimals] = useState();
+  const animalsFromLs: IAnimal[] = JSON.parse(
+    localStorage.getItem("animalList") || "[]"
+  );
+  const [animals, setAnimals] = useState<IAnimal[]>(animalsFromLs);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (animals.length > 0) {
+      return;
+    }
+    async function getAnimals() {
+      const response = await axios.get<IAnimal[]>(
+        "https://animals.azurewebsites.net/api/animals"
+      );
+      setAnimals(response.data);
+    }
+    getAnimals();
+  });
+
+  localStorage.setItem("animalList", JSON.stringify(animals));
+
+  function moreAnimalInfo(animalId: number) {
+    navigate("/animal/" + animalId);
+  }
 
   return (
     <>
       <h1>LandingPage</h1>
+
+      <section>
+        {animals.map((animal) => (
+          <div
+            className="animalContainer"
+            key={animal.id}
+            onClick={() => {
+              moreAnimalInfo(animal.id);
+            }}
+          >
+            <img src={animal.imageUrl} alt="animal" />
+            <h2>
+              {animal.name} ({animal.latinName})
+            </h2>
+          </div>
+        ))}
+      </section>
     </>
   );
 };
