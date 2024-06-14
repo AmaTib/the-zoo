68c641abe7be134505cd0c123fd948c8d49b9ba3
Amanda Tibell <141629199+AmaTib@users.noreply.github.com>
2024-06-14 11:43:01 +0200

    The oneAnimal page now shows one animal. Added a test function to test changing a property in the animal object

--- src/pages/OneAnimal.tsx
+++ src/pages/OneAnimal.tsx
@@ -1,7 +1,62 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { IAnimal } from "../models/IAnimal";
+
 export const OneAnimal = () => {
+  //hitta djuret med id från paramen från localstorage
+  const [animalsFromLs, setAnimalsFromLs] = useState<IAnimal[]>(
+    JSON.parse(localStorage.getItem("animalList") || "[]")
+  );
+  const { animalId } = useParams();
+  const navigate = useNavigate();
+  let pickedAnimal = null;
+
+  if (animalId) {
+    pickedAnimal = animalsFromLs.find((animal) => animal.id === +animalId);
+  }
+
+  function previousPage() {
+    navigate("/");
+  }
+
+  function changeAnimalPropertyTest(clickedAnimalId: number) {
+    setAnimalsFromLs(
+      animalsFromLs.map((animal) => {
+        if (animal.id === clickedAnimalId) {
+          return { ...animal, name: "abc" };
+        } else {
+          return animal;
+        }
+      })
+    );
+  }
+
+  useEffect(() => {
+    localStorage.setItem("animalList", JSON.stringify(animalsFromLs));
+  }, [animalsFromLs]);
+
   return (
     <>
-      <h1>One Animal</h1>
+      <button onClick={previousPage}>Tillbaka</button>
+      <h1>Mer om {pickedAnimal?.name}</h1>
+      <section>
+        <h2>{pickedAnimal?.latinName}</h2>
+        <img src={pickedAnimal?.imageUrl} alt={pickedAnimal?.name} />
+        <p>{pickedAnimal?.shortDescription}</p>
+        <div>
+          <p>{pickedAnimal?.lastFed}</p>
+          {pickedAnimal && (
+            <button
+              onClick={() => {
+                changeAnimalPropertyTest(pickedAnimal?.id);
+                console.log(animalsFromLs);
+              }}
+            >
+              Mata
+            </button>
+          )}
+        </div>
+      </section>
     </>
   );
 };
